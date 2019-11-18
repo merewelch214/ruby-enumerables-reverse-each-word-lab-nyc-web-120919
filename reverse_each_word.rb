0a9a4ca17fbfe348c0edb1052dbe1d9a789c7894
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-18 19:00:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 def reverse_each_word(sentence)
-  new_arr = []
   arr = sentence.split(" ")
-  arr.collect do
-    |word| word.reverse()
-  end
-  arr.join(" ")
+  arr.collect {|word| word.reverse }
   puts arr
 end
